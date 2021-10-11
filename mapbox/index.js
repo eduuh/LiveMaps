acef4b6c4867f397c1e59582e7e3cbca0c41c8cf
eduuh <edwinmuraya0@gmail.com>
2021-10-11 13:40:31 +0000

    feature. Added ability to toggle between the two layers
             Added schools on the map
    	 Added the shools for the whole map

--- mapbox/index.js
+++ mapbox/index.js
@@ -61,9 +61,15 @@ map.on("load", function () {
     data: "https://raw.githubusercontent.com/eduuh/mapboxsearch/master/mapbox/machashapefile.json?token=AHTOOWXSQ6VTV3YTLLXKJL3BNNBAU",
   })
 
+  //data: "https://mapboxsearch.azurewebsites.net/datasource/hospitals?distance=10000&longitude=37.26&latitude=-1.51543",
   map.addSource("hospitals", {
     type: "geojson",
-    data: "https://mapboxsearch.azurewebsites.net/datasource/hospitals?distance=10000&longitude=37.26&latitude=-1.51543",
+    data: "https://mapboxsearch.azurewebsites.net/datasource/hospitals",
+  })
+
+  map.addSource("schools", {
+    type: "geojson",
+    data: "https://mapboxsearch.azurewebsites.net/datasource/schools",
   })
   map.addControl(
     new MapboxGeocoder({
@@ -93,6 +99,21 @@ map.on("load", function () {
     id: "hospitallayer",
     type: "circle",
     source: "hospitals",
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "circle-color": "rgba(55,148,179,1)",
+    },
+  })
+
+  map.addLayer({
+    id: "SchoolLayer",
+    type: "circle",
+    source: "schools",
+    layout: {
+      visibility: "visible",
+    },
   })
 })
 
@@ -140,3 +161,50 @@ const coordinatesGeocoder = function (query) {
 
   return geocodes
 }
+
+// After the last frame rendered before the map enters an "idle" state.
+map.on("idle", () => {
+  // If these two layers were not added to the map, abort
+  if (!map.getLayer("hospitallayer") || !map.getLayer("SchoolLayer")) {
+    return
+  }
+
+  // Enumerate ids of the layers.
+  const toggleableLayerIds = ["hospitallayer", "SchoolLayer"]
+
+  // Set up the corresponding toggle button for each layer.
+  for (const id of toggleableLayerIds) {
+    // Skip layers that already have a button set up.
+    if (document.getElementById(id)) {
+      continue
+    }
+
+    // Create a link.
+    const link = document.createElement("a")
+    link.id = id
+    link.href = "#"
+    link.textContent = id
+    link.className = "active"
+
+    // Show or hide layer when the toggle is clicked.
+    link.onclick = function (e) {
+      const clickedLayer = this.textContent
+      e.preventDefault()
+      e.stopPropagation()
+
+      const visibility = map.getLayoutProperty(clickedLayer, "visibility")
+
+      // Toggle layer visibility by changing the layout object's visibility property.
+      if (visibility === "visible") {
+        map.setLayoutProperty(clickedLayer, "visibility", "none")
+        this.className = ""
+      } else {
+        this.className = "active"
+        map.setLayoutProperty(clickedLayer, "visibility", "visible")
+      }
+    }
+
+    const layers = document.getElementById("menu")
+    layers.appendChild(link)
+  }
+})
